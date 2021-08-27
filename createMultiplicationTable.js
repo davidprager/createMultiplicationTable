14bfed185a2aa65f9af06c08216deb410a6b5853
davidprager <david_prager@yahoo.com>
2021-08-26 21:20:41 -0400

    Initial commit of project

---
+++ createMultiplicationTable.js
@@ -0,0 +1,13 @@
+/* This program will create a multi-dimensional array simulating a multiplication table */
+
+"use strict"
+
+function createMultiplicationTable() {
+    let maxNumber = 9;
+    let multiplicationArray = [];
+    // Create array using two for loops
+
+    // Display multiplication table by traversing two arrays
+    // Build a line for each of the inner arrays and then output to console
+
+}
